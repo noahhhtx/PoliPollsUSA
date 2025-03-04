f2269c91231429dcdac9d4a31fed8ff4fbf99e5f
Noah Harrison <91997693+noahhhtx@users.noreply.github.com>
2025-03-04 01:17:45 -0600

    adding files...

---
+++ process_data.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import gspread as gsp
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
+
+def count_nones(x):
+    i = 0
+    k = None
+    for j in x:
+        if j is None:
+            i+=1
+        if j is not None:
+            k = j
+    return i, k
+
+def compute_conf_interval(responses, response):
+    z_val = 1.96 # 95% conf. interval, might want to change later
+    no_responses = len(responses)
+    no_response = len([x for x in responses if x == response])
+    proportion = 1.0 * no_response / no_responses
+    alt_proportion = 1 - proportion
+    MOE = z_val * np.sqrt( 1.0 * (proportion * alt_proportion) / no_responses )
+    return (np.round(100*proportion,4), np.round(100*(MOE),4))
+
+def compute_metrics(df, question, responses, dc):
+    df_copy = df.copy(deep=True)
+
+    demographic_string = ""
+    for i in range(len(dc)):
+        if dc[i] is None:
+            continue
+        df_copy = df_copy[df_copy[df_copy.columns[i]]==dc[i]]
+        if len(df_copy) <= 1:
+            return
+        demographic_string += (dc[i] + " ")
+    demographic_string = demographic_string.strip()
+    if demographic_string == "":
+        demographic_string = "Overall"
+
+    print(demographic_string)
+
+    props = []
+
+    q = df_copy.iloc[:, question]
+    for r in responses:
+        prop, prop_moe = compute_conf_interval(q, r)
+        print(r, prop, prop_moe)
+        props.extend([prop,prop_moe])
+    print()
+    return props, demographic_string
+
+gc = gsp.oauth()
+
+print("Obtaining titles")
+titles = [sh.title for sh in gc.openall() if sh.title.endswith("PoliPollsUSA")]
+print(titles)
+
+date = input("Select the date of the desired poll: ")
+date += " PoliPollsUSA"
+
+sh = gc.open(date).sheet1
+print("Sheet opened")
+data = sh.get_all_values()
+cols = data[0]
+data = data[1:]
+
+con = sqlite3.connect("PollingResults.db")
+
+df = pd.DataFrame(data)
+df.columns = cols
+
+df = df.drop("On a scale of 1-10, with 1 being bad and 10 being good, how confident are you in the stability of the U.S. economy under Donald Trump?", axis=1)
+
+print(df)
+
+print(len(df), "rows before preprocessing")
+
+# Preprocessing Part 1: We want only responses captured within a week of the first response
+df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+earliest_response = df["Timestamp"][0]
+print(earliest_response)
+latest_response = earliest_response + datetime.timedelta(weeks=1)
+print(latest_response)
+df = df[df["Timestamp"] <= latest_response]
+df = df.drop("Timestamp", axis=1)
+
+earliest_response_int = int(earliest_response.strftime("%Y%m%d"))
+
+# Preprocessing Part 2: Removing NA values
+df = df.replace("", np.nan)
+df = df.dropna()
+
+print(df.info())
+print(len(df), "rows after preprocessing")
+
+n_respondents = len(df)
+
+note = input("Notes about survey: ")
+
+print()
+
+# Now to generate the statistics...
+
+genders = [None] + list(pd.unique(df[df.columns[0]]))
+races = [None] + list(pd.unique(df[df.columns[1]]))
+parties = [None] + list(pd.unique(df[df.columns[2]]))
+
+print(genders)
+print(races)
+print(parties)
+
+overall_question_data = []
+col_names=None
+
+specific_data_for_convenience = {}
+
+for question in range(3, len(df.columns)):
+
+    specific_data_for_convenience[df.columns[question]] = {"Gender": {"Yes":[], "No":[], "Names":[]},
+                                                           "Party": {"Yes":[], "No":[], "Names":[]},
+                                                           "Race": {"Yes":[], "No":[], "Names":[]}}
+    answers = ["Yes", "No"]
+    col_names = ["Question"]
+    current_question_data = [df.columns[question]]
+    print(df.columns[question])
+
+    # Segment by demographics
+    for gender in genders:
+        for race in races:
+            for party in parties:
+                test = compute_metrics(df,question,answers, [gender,race,party])
+                if test is None:
+                    continue
+                results, demo_string = test[0], test[1]
+                current_question_data.extend(results)
+                i, k = count_nones([gender, race, party])
+                for ans in answers:
+                    a = demo_string + " " + ans
+                    b = a + " Margin of Error"
+                    col_names.extend([a,b])
+                if i == 2 and k is not None:
+                    if k in genders:
+                        specific_data_for_convenience[df.columns[question]]["Gender"]["Yes"].append(results[0])
+                        specific_data_for_convenience[df.columns[question]]["Gender"]["No"].append(results[2])
+                        specific_data_for_convenience[df.columns[question]]["Gender"]["Names"].append(k)
+                    elif k in parties:
+                        specific_data_for_convenience[df.columns[question]]["Party"]["Yes"].append(results[0])
+                        specific_data_for_convenience[df.columns[question]]["Party"]["No"].append(results[2])
+                        specific_data_for_convenience[df.columns[question]]["Party"]["Names"].append(k)
+                    else:
+                        specific_data_for_convenience[df.columns[question]]["Race"]["Yes"].append(results[0])
+                        specific_data_for_convenience[df.columns[question]]["Race"]["No"].append(results[2])
+                        specific_data_for_convenience[df.columns[question]]["Race"]["Names"].append(k)
+                if demo_string is not None and demo_string == "Overall":
+                    cursor = con.cursor()
+                    cursor.execute("""INSERT INTO survey_results (date, question, yes, yes_moe, no, no_moe, respondents, note) 
+                    VALUES(?,?,?,?,?,?,?,?)
+                    ON CONFLICT(date, question) DO UPDATE SET date=?, question=?, yes=?, yes_moe=?, no=?, no_moe=?, respondents=?, note=?;""", (earliest_response_int, df.columns[question], results[0],
+                                                  results[1], results[2], results[3], n_respondents, note, earliest_response_int, df.columns[question], results[0], results[1], results[2], results[3], n_respondents, note))
+                    con.commit()
+
+
+    overall_question_data.append(current_question_data)
+
+    print()
+
+overall_results = pd.DataFrame(overall_question_data, columns=col_names)
+print(overall_results)
+
+print(specific_data_for_convenience)
+
+for question in specific_data_for_convenience:
+    for indicator in specific_data_for_convenience[question]:
+        X = specific_data_for_convenience[question][indicator]["Names"]
+        yes = specific_data_for_convenience[question][indicator]["Yes"]
+        no = specific_data_for_convenience[question][indicator]["No"]
+        X_axis = np.arange(len(X))
+
+        plt.bar(X_axis - 0.2, yes, 0.4, label='Yes')
+        plt.bar(X_axis + 0.2, no, 0.4, label='No')
+
+        plt.xticks(X_axis, X)
+        plt.xlabel(indicator)
+        plt.ylabel("Percent")
+        plt.title(question + "\nBy " + indicator, fontsize=10)
+        plt.xticks(fontsize=7.5)
+        plt.ylim(0,100)
+        plt.legend()
+        plt.show()
+
+    #overall_results.to_csv("test.csv")
+
+con.close()
---
+++ website_backend.py
@@ -0,0 +1,127 @@
+# written by Noah Harrison
+
+from flask import Flask, request, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/query", methods =["GET", "POST"])
+def query():
+    query_string = ""
+    if request.method == "POST": # assumes there was in fact an input
+        query_strings=[]
+        text_box = request.form.get("keyword")
+        if text_box is not None and text_box != "":
+            keywords = []
+            for word in text_box.split(" "):
+                keywords.append("question LIKE " + "'%" + word + "%'")
+            query_strings.append(" AND ".join(keywords))
+        startdate = request.form.get("startdate")
+        if startdate is not None and startdate != "":
+            startdate = ("".join([x for x in startdate.split("-")]))
+            query_strings.append("date >= " + startdate)
+        enddate = request.form.get("enddate")
+        if enddate is not None and enddate != "":
+            enddate = ("".join([x for x in enddate.split("-")]))
+            query_strings.append("date <= " + enddate)
+        if len(query_strings) > 0:
+            query_string = " AND ".join(query_strings)
+            query_string = " WHERE " + query_string
+    if len(query_string) == 0:
+        query_string = " LIMIT 10" # implies nothing was inserted.
+    con = sqlite3.connect("PollingResults.db")
+    c = con.cursor()
+    output_html = '''
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+        * {
+            font-family: "Roboto", sans-serif;
+        }
+        table { margin-left: auto;
+            margin-right: auto; }
+        .left {float:left;}
+        .right {float:right;}
+        .center {text-align: center;}
+        #keyword { float:right}
+        #query_table { 
+            width:350px;
+         }
+        #results_table {
+            width:800px;
+        }
+        .results {
+            border:1px solid black;
+        }
+        form { text-align:center; }
+        h1 { text-align:center; }
+        hr.rounded {
+        border-top: 8px solid #bbb;
+        border-radius: 5px;
+        }
+        </style>
+        <body>
+        <h1>Previous Survey Results</h1>
+        <p style="text-align:center">Use this form to query previous survey results. </p>
+        <form action="/query" method="post">
+            <table id="query_table">
+            <tr>
+                <td class="left"> Keyword: </td> 
+                <td class="right"> <input type="text" id="keyword" name="keyword"/> </td>
+            </tr>
+            <tr>
+                <td class="left"> <label for="startdate">Earliest Poll Begin Date:</label> </td>
+                <td class="right"> <input type="date" id="startdate" name="startdate"/> </td>
+            </tr>
+            <tr>
+                <td class="left"> <label for="enddate">Latest Poll Begin Date:</label> </td>
+                <td class="right"> <input type="date" id="enddate" name="enddate"/> </td>
+            </tr>
+            <tr>
+                <td class="center"> <input type="submit" value="Submit"> </td>
+            </td>
+            </table>
+        </form>
+    '''
+    statement = ("SELECT date, question, yes, yes_moe, no, no_moe, respondents, note FROM survey_results"
+                 + query_string + ";")
+    print(statement)
+    result = c.execute(statement).fetchall()
+    if len(result) == 0:
+        output_html+= "<p style=\"text-align:center\">Sorry, no results could be found. Please try another query.</p>"
+    else:
+        output_html+='''<table class = "results" id = "results_table" style="text-align: left;">
+            <tr class = "results">
+                <th class = "results">Date</th>
+                <th class = "results">Question</th>
+                <th class = "results">Yes</th>
+                <th class = "results">Yes MOE</th>
+                <th class = "results">No</th>
+                <th class = "results">No MOE</th>
+                <th class = "results">n</th>
+                <th class = "results">Note</th>
+            </tr>'''
+        for row in result:
+            # adjust date
+            row = list(row)
+            temp = str(row[0])
+            temp = temp[4:6] + "/" + temp[6:] + "/" + temp[0:4]
+            row[0] = temp
+            temp_str = "<tr class = \"results\">"
+            for col in row:
+                temp_str += "<td class = \"results\">" + str(col) + "</td>"
+            temp_str += "</tr>"
+            output_html+=temp_str
+        output_html+="</table>"
+    output_html+='''
+    <br>
+    <form action="/">
+        <input type="submit" value="Home">
+    </form>
+    </body>'''
+    con.close()
+    return output_html
+    #return render_template("home.html")

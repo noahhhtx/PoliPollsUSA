0900a901db5314b368011533ddb47ad848afb158
Noah Harrison <91997693+noahhhtx@users.noreply.github.com>
2025-03-07 00:12:07 -0600

    updated more things

--- main.py
+++ main.py
@@ -12,22 +12,34 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/query", methods =["GET", "POST"])
 def query():
     query_string = ""
-    if request.method == "POST": # assumes there was in fact an input
+    if request.method == "POST" or request.method == "GET": # assumes there was in fact an input
+        text_box = None
+        startdate = None
+        enddate = None
+        if request.method == "POST":
+            text_box = request.form.get("keyword")
+            startdate = request.form.get("startdate")
+            enddate = request.form.get("enddate")
+        else:
+            text_box = request.args.get('keyword')
+            startdate = request.args.get('startdate')
+            enddate = request.args.get('enddate')
         query_strings=[]
-        text_box = request.form.get("keyword")
         if text_box is not None and text_box != "":
             keywords = []
             for word in text_box.split(" "):
                 keywords.append("question LIKE " + "'%" + word + "%'")
             query_strings.append(" AND ".join(keywords))
-        startdate = request.form.get("startdate")
         if startdate is not None and startdate != "":
             startdate = ("".join([x for x in startdate.split("-")]))
             query_strings.append("date >= " + startdate)
-        enddate = request.form.get("enddate")
         if enddate is not None and enddate != "":
             enddate = ("".join([x for x in enddate.split("-")]))
             query_strings.append("date <= " + enddate)
@@ -67,7 +79,7 @@ def query():
         <body>'''
     output_html += render_template("header.html")
     output_html += '''
-        <h1>Previous Survey Results</h1>
+        <h1 style="text-align:center">Previous Survey Results</h1>
         <p style="text-align:center">Use this form to query previous survey results.</p>
         <form action="/query" method="post">
             <table id="query_table">
